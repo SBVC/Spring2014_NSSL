5aa307a58a7b3fb2682aef9b858abd21feac7ebd
Paul Liverman <paul.liverman.iii@gmail.com>
2014-04-16 23:14:22 -0700

    added multiline input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #define DEBUG
+//#define DEBUGverbose
 
 #include <string>
 #include <iostream>
@@ -6,25 +7,35 @@
 using namespace std;
 
 int main() {
-	string line;
-	int key;
+	string line,data;
+	int key=0; //default key of zero
 
-	cout<<"Enter a line to encrypt:"<<endl;
-	getline(cin,line);
+	cout<<"Enter data to encrypt (hitting enter twice stops input):"<<endl;
+	do {
+		getline(cin,line);
+		data+=line;
+	}while(line.length()!=0);
 
 	cout<<endl<<"Enter an key (integer) to encrypt with: ";
 	cin>>key;
+	#ifdef DEBUG
+	cout<<"Key used: "<<key<<endl;
+	#endif // DEBUG
 
-	for (int i=0;i<line.size();i++)
-		line[i]^=key++;
+	for (int i=0;i<data.size();i++) {
+		data[i]^=key++;
+		#ifdef DEBUGverbose
+		cout<<"Char:"<<line[i]<<"\t"<<"Key:"<<key<<endl;
+		#endif // DEBUG
+	}
 
-	cout<<endl<<"Your encrypted line:"<<endl<<endl;
-	cout<<line;
+	cout<<endl<<"Your encrypted data:"<<endl<<endl;
+	cout<<data;
 
 	#ifdef DEBUG
 	cout<<endl<<endl;
 	system("pause");
-	#endif
+	#endif // DEBUG
 
 	return 0;
 }
