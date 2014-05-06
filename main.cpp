e8e9a77944e3b5cd8a301e0c9b61caaa05d94e26
Alofoxx <alofoxx@gmail.com>
2014-05-06 10:51:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <class.h>
+#include <ctime>
 using namespace std;
 
 int main() {
@@ -14,6 +15,8 @@ int main() {
 
 	cout<<"Welcome to Secret Note program."<<endl;
 
+	secretData::main()
+	
 	//this is where we would connect to a server/client or whatever
 	//cout<<"Enter a key for this communication: "; //this will be changed probably
 	//cin>>key; //this is a horrible way to get input but whatever
