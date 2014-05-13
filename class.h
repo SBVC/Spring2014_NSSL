90a88de7a1e24b15119a94e8f95e07ba1afdecbe
Patrick Gillespie Jr <alofoxx@gmail.com>
2014-05-13 09:42:17 -0700

    Added Alot

--- class.h
+++ class.h
@@ -3,51 +3,113 @@
 
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 using namespace std;
-
 class secretData{
 	protected:
-		short dataKey;
+		long dataKey;
+		short key;
 		string encryptedData;
 		int test;
+		int input;
+		string unencryptedData;
 	public:
-		bool encrypt(string input,short key);
+		bool encrypt(string input,long dataKey);
 		void printEncryptedData();
 		void printData();
 		void generateKey();
 		void setKey();
 		void main();
+		void choose();
+		void encryptMessage();
+		void decrypt();
 };
 
-void secretData::main{
+void secretData::main(){
 	//This is the start of the program
 	cout<<"Please enter 1 to generate a new key or 2 to enter your provided key:"<<endl;
 	cout<<"# "; //prompt for input
-	getline(cin,input);
+	cin >> input;
 	
 	switch (input)
+	{
 	case 1 : generateKey(); //this should call secretData::generateKey(); when 1 is entered
+		break;
 	case 2 : setKey(); //this should call secretData::setKey(); when 2 is entered
+		break;
    	default   : main(); //this should call secretData::main(); when anything else is entered
+	}
 }
 
-short secretData::generateKey(){
+void secretData::choose(){
+	//This is the start of the program
+	cout<<"Please enter 1 to encrypt a new message or 2 to decrypt a past message"<<endl;
+	cout<<"# "; //prompt for input
+	cin >> input;
+	
+	switch (input)
+	{
+	case 1 : encryptMessage(); //this should call secretData::encryptMessage(); when 1 is entered
+		break;
+	case 2 : decrypt(); //this should call secretData::decrypt(); when 2 is entered
+		break;
+   	default   : choose(); //this should call secretData::main(); when anything else is entered
+	}
+}
+void secretData::decrypt() {
+  string line;
+  ifstream myfile ("example.txt");
+  encryptedData = "";
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+      encryptedData += line;
+	}
+    myfile.close();
+  }
+  else cout << "Unable to open file"; 
+  encrypt(encryptedData,dataKey);
+  cout << encryptedData << endl;
+}
+
+void secretData::generateKey(){
 	//code should generate a number between 1111 and 9999
 	srand((unsigned)time(0)); 
 	short random_integer; 
 	short lowest=1111, highest=9999; 
 	short range=(highest-lowest)+1; 
 	random_integer = lowest+int(range*rand()/(RAND_MAX + 1.0)); 
-	dataKey = random_integer; //set dataKey to new random number
-	cout << "Your new key is now: " dataKey << endl; //tell user the new number
-	return null;
+	key = random_integer; //set dataKey to new random number
+	dataKey=(key * 1254785104821482321); 
+	cout << "Your new key is now: " << key << endl; //tell user the new number
+	cout << "Your real key is: " << dataKey << endl;
+	encryptMessage();
 }
 
+void secretData::encryptMessage(){
+	cout<<"Please enter your secret message."<<endl;
+	cout<<"# "; //prompt for input
+	cin >> unencryptedData;
+	encrypt(unencryptedData,dataKey);
+	cout << encryptedData << endl;
+	//cout << "test" << endl;
+	//encrypt(encryptedData,dataKey);
+	//cout << encryptedData << endl;
+
+  ofstream myfile;
+  myfile.open ("example.txt");
+  myfile << encryptedData; //<< "<<= \n The key is " << dataKey;
+  myfile.close();
+}
 void secretData::setKey(){
 	do {
 		cout<<"Enter your key now:" << endl;
 		cout<<"# "; //prompt for input
-		getline(cin,input);
+		cin >>input;
 
 		if (input >= 1111 ) {
 			if (input <= 9999) {
@@ -55,17 +117,17 @@ void secretData::setKey(){
 			}
 		}
 		else {
-		 cout<<"sorry your key is invalid. please try again!"
+		 cout<<"sorry your key is invalid. please try again!";
 		 test = 0;
 		}
 	}while(test=0);
-	dataKey=input;
-	cout<<"key accepted"
-	return = null;
-	
+	key=input;
+	dataKey=(key * 1254785104821482321); 
+	cout<<"key accepted";
+	choose();
 }
 
-bool secretData::encrypt(string input,short key){
+bool secretData::encrypt(string input,long dataKey){
 	dataKey=key; //save key before use
 	encryptedData=input;
 	for (int i=0;i<input.size();i++){
@@ -87,3 +149,14 @@ void secretData::printData(){
 		tmp[i]^=key++;
 	cout<<tmp<<endl;
 }
+/*
+string secretData::encrypt(){
+	do {
+		cout<<"# "; //prompt for input
+		getline(cin,input);
+
+		text.encrypt(input,key);
+		cout<<"  ";text.printEncryptedData(); //temporary
+		cout<<"  ";text.printData(); // temporary as well
+	}while(input.length()!=0); //continue until null input
+	}*/
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #define DEBUG
 //#define DEBUGverbose
 
-#include <string>
+
 #include <iostream>
 #include <cstdlib>
-#include <class.h>
+#include "class.h"
 #include <ctime>
 using namespace std;
 
@@ -14,21 +14,12 @@ int main() {
 	short key=0; //default key of zero
 
 	cout<<"Welcome to Secret Note program."<<endl;
-
-	secretData::main()
+	secretData sd;
+	sd.main();
 	
 	//this is where we would connect to a server/client or whatever
 	//cout<<"Enter a key for this communication: "; //this will be changed probably
 	//cin>>key; //this is a horrible way to get input but whatever
-
-	do {
-		cout<<"# "; //prompt for input
-		getline(cin,input);
-
-		text.encrypt(input,key);
-		cout<<"  ";text.printEncryptedData(); //temporary
-		cout<<"  ";text.printData(); // temporary as well
-	}while(input.length()!=0); //continue until null input
 
 	#ifdef DEBUG
 	cout<<endl<<endl;
