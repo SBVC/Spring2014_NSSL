3bf9c85e978112bbe04406a13edf7ca4baa0bab8
Paul Liverman <paul.liverman.iii@gmail.com>
2014-05-16 13:49:31 -0700

    Revert "Update class.h"
    This reverts commit cc1d87f8aa8c92308e8a071f6b3c29df548cade7.

--- class.h
+++ class.h
@@ -9,61 +9,11 @@ class secretData{
 	protected:
 		short dataKey;
 		string encryptedData;
-		int test;
 	public:
 		bool encrypt(string input,short key);
 		void printEncryptedData();
 		void printData();
-		void generateKey();
-		void setKey();
-		void main();
 };
-
-void secretData::main{
-	//This is the start of the program
-	cout<<"Please enter 1 to generate a new key or 2 to enter your provided key:"<<endl;
-	cout<<"# "; //prompt for input
-	getline(cin,input);
-	
-	switch (input)
-	case 1 : generateKey(); //this should call secretData::generateKey(); when 1 is entered
-	case 2 : setKey(); //this should call secretData::setKey(); when 2 is entered
-   	default   : main(); //this should call secretData::main(); when anything else is entered
-}
-
-short secretData::generateKey(){
-	//code should generate a number between 1111 and 9999
-	srand((unsigned)time(0)); 
-	short random_integer; 
-	short lowest=1111, highest=9999; 
-	short range=(highest-lowest)+1; 
-	random_integer = lowest+int(range*rand()/(RAND_MAX + 1.0)); 
-	dataKey = random_integer; //set dataKey to new random number
-	cout << "Your new key is now: " dataKey << endl; //tell user the new number
-	return null;
-}
-
-void secretData::setKey(){
-	do {
-		cout<<"Enter your key now:" << endl;
-		cout<<"# "; //prompt for input
-		getline(cin,input);
-
-		if (input >= 1111 ) {
-			if (input <= 9999) {
-				test = 1;
-			}
-		}
-		else {
-		 cout<<"sorry your key is invalid. please try again!"
-		 test = 0;
-		}
-	}while(test=0);
-	dataKey=input;
-	cout<<"key accepted"
-	return = null;
-	
-}
 
 bool secretData::encrypt(string input,short key){
 	dataKey=key; //save key before use
