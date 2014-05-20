c6daf5f7d630fc38de4d313e20bd12695b3cfa8f
Paul Liverman <paul.liverman.iii@gmail.com>
2014-05-19 18:59:57 -0700

    converted indentation to tabs

--- class.h
+++ class.h
@@ -9,60 +9,61 @@
 using namespace std;
 
 class NSSL{
-    protected:
-        string dataKey;
-        string encryptedData;
-    public:
-        NSSL(string input,long key);
-        void encrypt(string input,long key);
-        void rawInput(string input);
-        string decrypt();
-        void printEncrypted();
-        void printDecrypted();
-        static long generateKey();
+	protected:
+		string dataKey;
+		string encryptedData;
+	public:
+		NSSL(){}
+		NSSL(string input,long key);
+		void encrypt(string input,long key);
+		void addData(string input);
+		string decrypt();
+		void printEncrypted();
+		void printDecrypted();
+		static long generateKey();
 };
 
 NSSL::NSSL(string input,long key){
-    encrypt(input,key);
+	encrypt(input,key);
 }
 void NSSL::encrypt(string input,long key){
-    ostringstream long2string;
-    long2string<<key;
-    dataKey=long2string.str();
-    encryptedData=input;
-    for (int i=0;i<input.size();i++){
-        for (int j=0;j<dataKey.size();j++){
-            encryptedData[i]^=dataKey[j]^i;
-            i++;
-            if (i==input.size()) return;
-        }
-    }
+	ostringstream long2string;
+	long2string<<key;
+	dataKey=long2string.str();
+	encryptedData=input;
+	for (int i=0;i<input.size();i++){
+		for (int j=0;j<dataKey.size();j++){
+			encryptedData[i]^=dataKey[j]^i;
+			i++;
+			if (i==input.size()) return;
+		}
+	}
 }
-void NSSL::rawInput(string input){
-    encryptedData+=input;
+void NSSL::addData(string input){
+	encryptedData+=input;
 }
 string NSSL::decrypt(){
-    string tmp=encryptedData;
-    for (int i=0;i<encryptedData.size();i++){
-        for (int j=0;j<dataKey.size();j++){
-            tmp[i]^=dataKey[j]^i;
-            i++;
-            if (i==encryptedData.size()) return tmp;
-        }
-    }
-    return tmp;
+	string tmp=encryptedData;
+	for (int i=0;i<encryptedData.size();i++){
+		for (int j=0;j<dataKey.size();j++){
+			tmp[i]^=dataKey[j]^i;
+			i++;
+			if (i==encryptedData.size()) return tmp;
+		}
+	}
+	return tmp;
 }
 void NSSL::printEncrypted(){
-    cout<<encryptedData<<endl;
+	cout<<encryptedData<<endl;
 }
 void NSSL::printDecrypted(){
-    cout<<decrypt()<<endl;
+	cout<<decrypt()<<endl;
 }
 long NSSL::generateKey(){
-    srand(time(NULL));
-    long key=rand()*1254785104821482321;
-    #ifdef DEBUG
-    cout<<key<<endl;
-    #endif // DEBUG
-    return key;
+	srand(time(NULL));
+	long key=rand()*1254785104821482321;
+	#ifdef DEBUG
+	cout<<key<<endl;
+	#endif // DEBUG
+	return key;
 }
--- main.cpp
+++ main.cpp
@@ -12,62 +12,66 @@ int main();
 bool openFileAndDecrypt(string filename);
 
 int main() {
-    char in;
+	char in;
 	string input;
 	string filename;
 	long key;
 
+	NSSL data;
+
 	cout<<"Welcome to Secret Note program."<<endl;
 
 	do {
-        //this is ugly and should be rewritten better
-        cout<<"(G)enerate a key or (E)nter a key?"<<endl;
-        in=getch();
-        switch(in){
-            case 'g': case 'G':
-                key=NSSL::generateKey();
-                cout<<"Your key is: "<<key<<"\n";
-                in='0';
-                break;
-            case 'e': case 'E':
-                cout<<"Enter your key: ";
-                cin>>key;
-                in='0';
-                break;
-            case '0':
-                in='n'; //something other than 0 to keep user from skipping
-            default:
-                /*do nothing actually*/;
-        }
+		//this is ugly and should be rewritten better
+		cout<<"(G)enerate a key or (E)nter a key?"<<endl;
+		in=getch();
+		switch(in){
+			case 'g': case 'G':
+				data.generateKey();
+				cout<<"Your key is: "<<key<<"\n";
+				in='0';
+				break;
+			case 'e': case 'E':
+				cout<<"Enter your key: ";
+				cin>>key;
+				in='0';
+				break;
+			case '0':
+				in='n'; //something other than 0 to keep user from skipping
+			default:
+				/*do nothing actually*/;
+		}
 	}while(in!='0');
 
 	do {
-        cout<<"(D)ecrypt from file or (E)ncrypt to file? [0 to exit]"<<endl;
-        in=getch();
-        switch(in){
-            case 'd': case 'D':
-                neverUseLabelsAndGotos:
-                cout<<"Enter file name: ";
-                getline(cin,filename);
-                if (openFileAndDecrypt(filename)) {
-                    in='0';}
-                else {
-                    goto neverUseLabelsAndGotos;
-                }
-                break;
-            case 'e': case 'E':
-                //make ending done with "/done" on a line by itself instead of an empty line
-                //prompt (as below) for text until empty message,
-                //then encrypt and output to a file
-                in='0';
-                break;
-            default:
-                /*do nothing actually*/;
-        }
+		cout<<"(D)ecrypt from file or (E)ncrypt to file? [0 to exit]"<<endl;
+		in=getch();
+		switch(in){
+			case 'd': case 'D':
+				neverUseLabelsAndGotos:
+				cout<<"Enter file name: ";
+				getline(cin,filename);
+				if (openFileAndDecrypt(filename)) {
+					in='0';}
+				else {
+					goto neverUseLabelsAndGotos;
+				}
+				break;
+			case 'e': case 'E':
+				//make ending done with "/done" on a line by itself instead of an empty line
+				//prompt (as below) for text until empty message,
+				//then encrypt and output to a file
+				in='0';
+				break;
+			default:
+				/*do nothing actually*/;
+		}
 	}while(in!='0');
 
-	//at this point we need a pause so a decrypted file will stay open
-	//or a message about succesfful encryption and saving to file needs to be displayed
+	cout<<"\n\n"<<"Press any key to exit . . . "<<endl;
+	getch();
+
+	//THIS IS EXIT OF PROGRAM, REMOVE BELOW WHEN IT HAS BEEN ADDED TO APPROPRIATE PLACE
 
 	do {
 		cout<<"# "; //prompt for input
@@ -82,36 +86,29 @@ int main() {
 
 	}while(input.size()!=0); //continue until null input
 
-	#ifdef DEBUG
-	cout<<"\n"<<endl;
-	//system("pause"); //replaced with coded version cause Linux support
-	cout<<"Press any key to continue . . . ";
-	getch();
-	#endif // DEBUG
-
 	return 0;
 }
 
 bool openFileAndDecrypt(string filename){
-    NSSL data("blah",123); //this is bullshit
-    //this assumes data is defined and accessible to it
-    //it probably is neither, rewrite class to allow constructing uninitialized object
-    //make data at beginning and accessible to everything
-    string line;
-    ifstream file(filename.c_str());
-    //ifstream file;
-    //file.open(filename);
-    //ifstream file("test.txt");
-    if (file.is_open()){
-        while(getline(file,line)){
-            //add to encrypted data
-            data.rawInput(line); //this may need a +"\n" added to it to work properly
-        }
-        file.close();
-        //decrypt and display
-        data.printDecrypted();
-        return true;
-    }
-    cout<<"Error: File did not open."<<endl;
-    return false;
+	NSSL data("blah",123); //this is bullshit
+	//this assumes data is defined and accessible to it
+	//it probably is neither, rewrite class to allow constructing uninitialized object
+	//make data at beginning and accessible to everything
+	string line;
+	ifstream file(filename.c_str());
+	//ifstream file;
+	//file.open(filename);
+	//ifstream file("test.txt");
+	if (file.is_open()){
+		while(getline(file,line)){
+			//add to encrypted data
+			data.addData(line); //this may need a +"\n" added to it to work properly
+		}
+		file.close();
+		//decrypt and display
+		data.printDecrypted();
+		return true;
+	}
+	cout<<"Error: File did not open."<<endl;
+	return false;
 }
